58faf39eaf9ae84ff333113d7ab2f12b5da5ab33
justinmajetich <justinmajetich@gmail.com>
2020-03-23 21:14:52 +0000

    Add comment

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,5 +1,5 @@
-file { 'holberton':
-  path    => '/tmp/holberton',
+# Creates a file with content
+file { '/tmp/holberton':
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
