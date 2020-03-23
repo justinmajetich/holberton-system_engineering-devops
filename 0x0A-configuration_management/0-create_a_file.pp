c304dcec7122502d0dbe7da88393fc5210f613f4
justinmajetich <justinmajetich@gmail.com>
2020-03-23 21:08:59 +0000

    Re-order attributes

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 file { 'holberton':
   path    => '/tmp/holberton',
-  content => 'I love Puppet',
+  mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  mode    => '0744'
+  content => 'I love Puppet',
 }
