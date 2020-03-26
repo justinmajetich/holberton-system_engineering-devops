2b30452ee048411e7fd0f1511f3bb13671c39eaf
justinmajetich <justinmajetich@gmail.com>
2020-03-26 02:32:24 +0000

    Write puppet nginx configuration

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,18 @@
+# Install and configure nginx
+include nginx
+
+class { 'nginx':
+  manage_repo    => true,
+  package_source => 'nginx-stable',
+}
+
+nginx::resource::server { '34.73.76.135':
+  listen_port      => 80,
+  www_root         => '/var/www/html/'
+  vhost_cfg_append => { 'rewrite' => '^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent' },
+}
+
+file { 'index':
+  path    => '/var/www/html/index.nginx-debian.html',
+  content => 'Holberton School for the win!',
+}
