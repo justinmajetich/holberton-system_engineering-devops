30bd943b4994b3aef276148045d9a508a0fb8503
justinmajetich <justinmajetich@gmail.com>
2020-04-03 21:38:19 +0000

    Use nginx variables for hostname

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -5,5 +5,5 @@ file_line { 'custom response header':
   ensure => present,
   path   => '/etc/nginx/sites-available/default',
   after  => '        error_page 404 /custom_404.html;',
-  line   => "        add_header X-Served-By ${hostname};",
+  line   => '        add_header X-Served-By $hostname;',
 }
