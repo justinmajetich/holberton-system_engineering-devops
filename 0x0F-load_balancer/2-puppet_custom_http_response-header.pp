5f66a16a47ddf7cf9dacf79c5c9ca24af1610056
justinmajetich <justinmajetich@gmail.com>
2020-04-03 23:27:42 +0000

    Include all config code

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,9 +1,6 @@
 # Configures a custom response header
-include stdlib
 
-file_line { 'custom response header':
-  ensure => present,
-  path   => '/etc/nginx/sites-available/default',
-  after  => '        error_page 404 /custom_404.html;',
-  line   => '        add_header X-Served-By $hostname;',
+exec { 'create index':
+  command  => 'INDEX_COPY="Holberton School for the win!" && ERROR_COPY="Ceci n\'est pas une page - 404" && sudo apt-get -y update && sudo apt-get -y install nginx && echo "$INDEX_COPY" | sudo tee /var/www/html/index.nginx-debian.html > /dev/null && echo "$ERROR_COPY" | sudo tee /var/www/html/custom_404.html > /dev/null && sudo sed -i \'/^\sserver_name.*/a \        rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;\' /etc/nginx/sites-available/default && sudo sed -i \'/^\slocation.*/i \        error_page 404 /custom_404.html;\' /etc/nginx/sites-available/default && sudo sed -i \'/^\slocation.*/i \        add_header X-Served-By $hostname;\' /etc/nginx/sites-available/default && sudo service nginx start',
+  provider => shell,
 }
