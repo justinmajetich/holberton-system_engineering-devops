41d342d76db0c994068fcea01d59216a3174932b
justinmajetich <justinmajetich@gmail.com>
2020-05-20 16:18:39 +0000

    Simplify sed command

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # Fix typo in WordPress settings file
 exec { 'Fix typo in filename':
-  command  => 'sudo sed -i "137s@.*@require_once( ABSPATH . WPINC . \'/class-wp-locale.php\' );@" /var/www/html/wp-settings.php',
+  command  => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
   provider => shell,
 }
