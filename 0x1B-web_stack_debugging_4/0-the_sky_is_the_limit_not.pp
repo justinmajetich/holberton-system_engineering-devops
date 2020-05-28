f9b59f2cd960298aa22eff0510d70cd2aa77d091
justinmajetich <justinmajetich@gmail.com>
2020-05-28 20:14:24 +0000

    Add commands to change system-wide limits

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,4 +1,4 @@
 exec { 'Change nginx limit':
-  command  => 'sudo sed -i "s/^ULIMIT.*$/ULIMIT=\"-n 4096\"/" /etc/default/nginx && sudo service nginx restart',
+  command  => 'sudo sed "55 i nginx\tsoft\tnofile\t10048\nnginx\thard\tnofile\t30048" /etc/security/limits.conf && sudo sed -i "$ a\fs.file-max = 100000" /etc/sysctl.conf && sudo sed -i "s/^ULIMIT.*$/ULIMIT=\"-n 4096\"/" /etc/default/nginx && sudo sysctl -p && sudo service nginx restart',
   provider => shell,
 }
