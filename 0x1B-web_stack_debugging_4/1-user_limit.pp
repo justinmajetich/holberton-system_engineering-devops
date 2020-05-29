b9f9b875fc79831a846725d5fb4ee2290285194e
justinmajetich <justinmajetich@gmail.com>
2020-05-29 15:03:45 +0000

    Narrow limit changes to just limits.conf

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,5 +1,10 @@
 # change system wide limits
-exec { 'Change nginx limit':
-  command  => 'sudo sed "55 i holberton\tsoft\tnofile\t10048\nholberton\thard\tnofile\t30048" /etc/security/limits.conf && sudo sed -i "$ a\fs.file-max = 100000" /etc/sysctl.conf && sudo sed -i "s/^ULIMIT.*$/ULIMIT=\"-n 4096\"/" /etc/default/nginx && sudo sysctl -p && sudo service nginx restart',
+exec { 'Change soft limit':
+  command  => 'sudo sed -i "s/holberton\ssoft.*/holberton\tsoft\tnofile\t10000/" /etc/security/limits.conf',
+  provider => shell,
+}
+
+exec { 'Change hard limit':
+  command  => 'sudo sed -i "s/holberton\shard.*/holberton\thard\tnofile\t100000/" /etc/security/limits.conf',
   provider => shell,
 }
